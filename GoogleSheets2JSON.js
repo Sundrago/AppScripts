b35e78b8b6e3d218b1fcf095cfb8c2e076394ef9
SunDrargon <88926252+Sundrago@users.noreply.github.com>
2024-03-18 19:18:20 +0900

    Update GoogleSheets2JSON.js

--- GoogleSheets2JSON.js
+++ GoogleSheets2JSON.js
@@ -1,36 +1,37 @@
-function ConvertDataIntoJson() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  var lastColumn = sheet.getLastColumn();
-  var lastRow = sheet.getLastRow();
+/**
+ * Converts spreadsheet data into a structured JSON object.
+ */
+function convertDataIntoJson() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  const {lastColumn, lastRow} = {lastColumn: sheet.getLastColumn(), lastRow: sheet.getLastRow()};
   
-  var properties = sheet.getRange(1, 2, 1, lastColumn - 1).getValues()[0];
-  var dataTypes = sheet.getRange(3, 2, 1, lastColumn - 1).getValues()[0];
-  var inclusionCriteria = sheet.getRange(2, 2, 1, lastColumn - 1).getValues()[0];
-  var dataRows = sheet.getRange(4, 2, lastRow - 3, lastColumn - 1).getValues();
-  var keys = sheet.getRange(4, 1, lastRow - 3, 1).getValues();
+  const properties = sheet.getRange(1, 2, 1, lastColumn - 1).getValues()[0];
+  const dataTypes = sheet.getRange(3, 2, 1, lastColumn - 1).getValues()[0];
+  const inclusionCriteria = sheet.getRange(2, 2, 1, lastColumn - 1).getValues()[0];
+  const dataRows = sheet.getRange(4, 2, lastRow - 3, lastColumn - 1).getValues();
+  const keys = sheet.getRange(4, 1, lastRow - 3, 1).getValues();
 
-  var dictionary = {};
+  let dictionary = {};
 
-  keys.forEach(function(keyRow, rowIndex) {
-    var key = keyRow[0];
+  keys.forEach((keyRow, rowIndex) => {
+    const key = keyRow[0];
     if (key !== "" && !isNaN(key)) {
-      var obj = {};
-      var arrays = {};
+      let obj = {};
+      let arrays = {};
 
-      properties.forEach(function(property, propIndex) {
+      properties.forEach((property, propIndex) => {
         if (inclusionCriteria[propIndex] === "all") {
-          var value = dataRows[rowIndex][propIndex];
-          var dataType = dataTypes[propIndex];
+          const value = dataRows[rowIndex][propIndex];
+          const dataType = dataTypes[propIndex];
 
           if (property.includes('[') && property.includes(']')) { 
-            var arrayName = property.split('[')[0]; 
-            var arrayIndex = parseInt(property.match(/\[(\d+)\]/)[1], 10); 
+            const arrayName = property.split('[')[0]; 
+            const arrayIndex = parseInt(property.match(/\[(\d+)\]/)[1], 10);
 
-            if (!arrays[arrayName]) arrays[arrayName] = []; 
-            if (dataType === 'int[]' && value !== "") arrays[arrayName][arrayIndex] = parseInt(value, 10); 
-            else if (value !== "") arrays[arrayName][arrayIndex] = value; 
-
-          } else if (value !== "") { 
+            arrays[arrayName] = arrays[arrayName] || []; 
+            if (dataType === 'int[]' && value !== "") arrays[arrayName][arrayIndex] = parseInt(value, 10);
+            else if (value !== "") arrays[arrayName][arrayIndex] = value;
+          } else if (value !== "") {
             obj[property] = dataType === 'int' ? parseInt(value, 10) : value;
           }
         }
@@ -44,37 +45,42 @@ function ConvertDataIntoJson() {
     }
   });
 
-  var jsonString = JSON.stringify(dictionary, null, 2);
+  const jsonString = JSON.stringify(dictionary, null, 2);
   Logger.log(jsonString);
-  ShowCopyDialog(jsonString); 
+  createJsonFile(jsonString, "exportedData.json"); // Changed to automatically save the JSON and show in dialog
 }
 
-function ShowCopyDialog(jsonString, url) {
-    var htmlOutput = HtmlService
-        .createHtmlOutput('<textarea id="jsonText" style="width:100%;height:200px;">'
-                          + jsonString +
-                          '</textarea><br><button onclick="copyToClipboard()">Copy to Clipboard</button>'
-                          + '<button onclick="window.open(\'' + url + '\');">Download JSON</button>'
-                          + '<script>function copyToClipboard() {var copyText = document.getElementById("jsonText"); copyText.select(); document.execCommand("copy"); alert("Copied!");}</script>')
-        .setWidth(400)
-        .setHeight(250)
-        .setTitle('Copy JSON & Download JSON');
+/**
+ * Displays a dialog with options to copy and download the JSON string.
+ * @param {string} jsonString - JSON string to display.
+ * @param {string} [url] - URL of the downloadable JSON file.
+ */
+function showCopyDialog(jsonString, url) {
+    const htmlTemplate = HtmlService.createTemplateFromFile('DialogTemplate');
+    htmlTemplate.jsonString = jsonString;
+    htmlTemplate.url = url;
+    const htmlOutput = htmlTemplate.evaluate().setWidth(400).setHeight(250).setTitle('Copy JSON & Download JSON');
     SpreadsheetApp.getUi().showModalDialog(htmlOutput, 'Copy JSON & Download JSON');
 }
 
-function CreateJsonFile(jsonString, fileName) {
-  // Create a file in Google Drive with the JSON content
-  var file = DriveApp.createFile(fileName, jsonString, MimeType.PLAIN_TEXT);
-  
+/**
+ * Creates a JSON file in Google Drive and opens a dialog for copying and downloading.
+ * @param {string} jsonString - JSON content to save.
+ * @param {string} fileName - name for the new file.
+ */
+function createJsonFile(jsonString, fileName) {
+  const file = DriveApp.createFile(fileName, jsonString, MimeType.PLAIN_TEXT);
   file.setSharing(DriveApp.Access.ANYONE_WITH_LINK, DriveApp.Permission.VIEW);
-  ShowCopyDialog(jsonString, file.getUrl());
+  showCopyDialog(jsonString, file.getUrl());
   Logger.log(file.getUrl());
 }
 
+/**
+ * Adds a custom menu to the Google Sheets
+ */
 function onOpen() {
-  var ui = SpreadsheetApp.getUi();
-  // Or DocumentApp, SlidesApp or FormApp.
+  const ui = SpreadsheetApp.getUi();
   ui.createMenu('선용툴')
-      .addItem('Json 생성', 'ConvertDataIntoJson')
-      .addToUi();
+    .addItem('Json 생성', 'convertDataIntoJson')
+    .addToUi();
 }
